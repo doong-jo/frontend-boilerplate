14956340ef3c914dc92ccdd27d37970386380139
doong-jo <sdong001@gmail.com>
2019-12-31 12:31:14 +0900

    refactor: refact nomalize usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import 'App.css';
+import GlobalStyles from './commons/style/globalstyle';
+import defaultTheme from 'commons/style/themes/default';
 
 const App: React.FC = () => {
-  return <h1>Hello Frontend Workspace!</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <div style={{ textAlign: 'center', marginTop: '10rem' }}>
+        <h3>Welcome frontend boilerplate :)</h3>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
