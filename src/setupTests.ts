2dfafec081951f0a2ddec3be7b5f1f3f216b8747
doong-jo <sdong001@gmail.com>
2020-01-21 18:26:20 +0900

    build: switch enzyme with react-testing-library

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,1 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'intersection-observer';
-
-configure({ adapter: new Adapter() });
+import '@testing-library/jest-dom/extend-expect';
